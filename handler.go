2cf686ee8bf43c6fe657713d707388ede367ca4f
Guilherme Santos <guilherme.santos@neoway.com.br>
2015-08-21 16:17:47 -0300

    Set writer depending of level setted

--- handler.go
+++ handler.go
@@ -1,6 +1,8 @@
 package logger
 
 import (
+	"io"
+	"io/ioutil"
 	"log"
 	"os"
 )
@@ -20,9 +22,20 @@ func (handler *DefaultHandler) Init(namespace string, level Level) {
 		namespace = "<" + namespace + "> "
 	}
 
-	handler.DebugLogger = log.New(os.Stdout, namespace+"[DEBUG] ", 0)
-	handler.InfoLogger = log.New(os.Stdout, namespace+"[INFO] ", 0)
-	handler.WarnLogger = log.New(os.Stdout, namespace+"[WARN] ", 0)
+	var debugOutput, infoOutput, warnOutput io.Writer
+	if level == LevelDebug {
+		debugOutput, infoOutput, warnOutput = os.Stdout, os.Stdout, os.Stdout
+	} else if level == LevelDebug {
+		debugOutput = ioutil.Discard
+		infoOutput, warnOutput = os.Stdout, os.Stdout
+	} else {
+		debugOutput, infoOutput = ioutil.Discard, ioutil.Discard
+		warnOutput = os.Stdout
+	}
+
+	handler.DebugLogger = log.New(debugOutput, namespace+"[DEBUG] ", 0)
+	handler.InfoLogger = log.New(infoOutput, namespace+"[INFO] ", 0)
+	handler.WarnLogger = log.New(warnOutput, namespace+"[WARN] ", 0)
 	handler.ErrorLogger = log.New(os.Stderr, namespace+"[ERROR] ", 0)
 	handler.FatalLogger = log.New(os.Stderr, namespace+"[FATAL] ", 0)
 }
